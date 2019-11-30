aa5cf25055b558a953f80313bdebe4aa42a1058b
hayashi-ay <a_hayashi@codmon.co.jp>
2019-11-30 21:32:22 +0900

    implement search_venues()

--- app.py
+++ app.py
@@ -75,17 +75,14 @@ def venues():
 
 @app.route('/venues/search', methods=['POST'])
 def search_venues():
-  # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-  # seach for Hop should return "The Musical Hop".
-  # search for "Music" should return "The Musical Hop" and "Park Square Live Music & Coffee"
-  response={
-    "count": 1,
-    "data": [{
-      "id": 2,
-      "name": "The Dueling Pianos Bar",
-      "num_upcoming_shows": 0,
-    }]
-  }
+  # TODO: get shows info
+  search_term = request.form.get('search_term' )
+  data = Venue.query.filter( Venue.name.ilike( '%{}%'.format( search_term) ) ).all()
+
+  response = {}
+  response['count'] = len( data )
+  response['data'] = data
+
   return render_template('pages/search_venues.html', results=response, search_term=request.form.get('search_term', ''))
 
 @app.route('/venues/<int:venue_id>')
